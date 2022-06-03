552d59d3a626c4c14fb679823dd2d9479abe0ae5
Ajesh Anilan <ajesh.anilan@akqa.com>
2022-06-03 16:51:16 +0530

    heading update

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Your Name';
+const name = 'Ajesh Next App';
 export const siteTitle = 'Next.js Test Website';
 
 export default function Layout({ children, home }) {
